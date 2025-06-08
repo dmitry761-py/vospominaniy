121f9a854c0bcaeea965aa47592da617edfd988c
Dmitry <terd144@yandex.ru>
2025-06-08 20:19:56 +0200

    seatch5

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Папка для сохранения фото на диске D:
-const uploadDir = 'D:/photo';
+const uploadDir = 'E:/photo';
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
 
 app.use(express.static(path.join(__dirname, '../public')));
@@ -15,7 +15,7 @@ app.post('/upload', async (req, res) => {
   try {
     const imageBase64 = req.body.image.split(',')[1];
     const buffer = Buffer.from(imageBase64, 'base64');
-    const filename = 'photo_${Date.now()}.jpg';
+    const filename = `photo_${Date.now()}.jpg`;
     const filepath = path.join(uploadDir, filename);
 
     fs.writeFileSync(filepath, buffer);
